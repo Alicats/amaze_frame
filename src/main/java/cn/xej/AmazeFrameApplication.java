548218378f62281ae8c1cd0975b314ca5ef829d2
tom_glb <goodMorning_glb@xej.com>
2020-07-06 13:10:09 +0800

    amaze整合freemarker

---
+++ src/main/java/cn/xej/AmazeFrameApplication.java
@@ -0,0 +1,13 @@
+package cn.xej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazeFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazeFrameApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/xej/controller/LoginController.java
@@ -0,0 +1,12 @@
+package cn.xej.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class LoginController {
+
+
+}
